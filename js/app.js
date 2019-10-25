977c8613f09111b38b535285e200376f17e3b44e
cosmin-is <52592282+cosmin-is@users.noreply.github.com>
2019-10-25 12:15:59 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//Butoane active meniu
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//Butoane active portofoliu
+var portofoliu = document.getElementById("navlist");
+var link = portofoliu.getElementsByClassName("btn");
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
